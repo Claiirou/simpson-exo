6ec396e90a4d78c4668ff02b3acbacdb3295c9fd
Claiirou <clairemachet@hotmail.fr>
2022-04-09 13:11:26 +0200

    try 3

--- src/components/Simpson.js
+++ src/components/Simpson.js
@@ -1,20 +1,42 @@
 import React from "react";
 import axios from "axios";
 
-const [Quote, setQuote] = React.useState(setQuote)
+export default class QuoteList extends React.Component{
 
-const Simpson() = {
-    axios
-    .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
-    .then((response) => response.data)
-    .then((data) => {
+    state = {
+        quote: [],
+    };
 
-      setQuote(data.results[0]);
-
+componentDidMount() {
+    axios.get(`https://simpsons-quotes-api.herokuapp.com/quotes?count=10`)
+    .then(res => {
+        console.log(res);
+        this.setState({quote: res.data});
     });
+}
+
+render(){
+    return(
+        <ul>
+            {this.state.quote.map(quote => <li>{quote.name}</li>)}
+        </ul>
+    )
+}
 
 }
 
 
 
-export default Simpson; 
+
+/*const [Quote, setQuote] = React.useState(setQuote)
+const Simpson() = {
+    axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
+    .then((response) => response.data)
+    .then((data) => {
+      setQuote(data.results[0]);
+    });
+}*/
+
+
+
