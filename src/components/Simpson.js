43b32f8503d162e79350386d588cd61a9704d516
Claiirou <clairemachet@hotmail.fr>
2022-04-09 12:57:59 +0200

    try 2

--- src/components/Simpson.js
+++ src/components/Simpson.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-const SimpsonQuote [Quote, SetQuote] = React.useState()
+const [Quote, setQuote] = React.useState(setQuote)
 
 const Simpson() = {
     axios
