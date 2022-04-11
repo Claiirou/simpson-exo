ae298862f90d657c66616b2e0f81fa1953163012
Claiirou <clairemachet@hotmail.fr>
2022-04-11 15:32:14 +0200

    finish exo

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Simpson from './components/Simpson';
+import axios from 'axios';
 
-const quoteSimpson = {
-
+/*const quoteSimpson = {
   character: "bart",
   quote: 'hello',
-  }
+  }*/
 
 function App() {
-  const [quote, setQuote] = React.useState(setQuote);
+  const [quote, setQuote] = React.useState("");
+
+  const getQuote = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')  
+      .then((response) => response.data)  
+      .then((data) => {
+        console.log(data);
+        setQuote(data[0]);
+      });
+  };
 
   return (
     <div className="App">
-    <Simpson quote={quoteSimpson}/>
+    <Simpson quote={quote}/>
+    <button type="button" onClick={getQuote}>New Quote</button>
     </div>
 
   );
--- src/components/Simpson.js
+++ src/components/Simpson.js
@@ -1,14 +1,14 @@
 import React from "react";
-import axios from "axios";
 
 
 const Simpson = ({quote}) => {
-    return (
+    return ( quote && (
 <div>
     <p>{quote.quote}</p>
     <p>{quote.character}</p>
     <img src={quote.image} alt={quote.character}></img>
 </div>
+    )
 );
 }
 
