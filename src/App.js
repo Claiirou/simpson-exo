1ed1f1df2e2e84434c6b199ff18d63fa08f9864f
Claiirou <clairemachet@hotmail.fr>
2022-04-11 14:05:51 +0200

    modify

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 import React from 'react';
 import Simpson from './components/Simpson';
 
+const quoteSimpson = {
+
+  character: "bart",
+  quote: 'hello',
+  }
+
 function App() {
+  const [quote, setQuote] = React.useState(setQuote);
+
   return (
-    <Simpson />
+    <div className="App">
+    <Simpson quote={quoteSimpson}/>
+    </div>
+
   );
 }
 
--- src/components/Simpson.js
+++ src/components/Simpson.js
@@ -1,42 +1,16 @@
 import React from "react";
 import axios from "axios";
 
-export default class QuoteList extends React.Component{
 
-    state = {
-        quote: [],
-    };
-
-componentDidMount() {
-    axios.get(`https://simpsons-quotes-api.herokuapp.com/quotes?count=10`)
-    .then(res => {
-        console.log(res);
-        this.setState({quote: res.data});
-    });
-}
-
-render(quote, character, image){
-    return(
-        <ul>
-            {this.state.quote.map(quote => (
-            <li key={quote.id}>{quote.quote}{quote.character}{quote.image}</li>))}
-        </ul>
-    );
-}
+const Simpson = ({quote}) => {
+    return (
+<div>
+    <p>{quote.quote}</p>
+    <p>{quote.character}</p>
+    <img src={quote.image} alt={quote.character}></img>
+</div>
+);
 }
 
 
-
-
-/*const [Quote, setQuote] = React.useState(setQuote)
-const Simpson() = {
-    axios
-    .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
-    .then((response) => response.data)
-    .then((data) => {
-      setQuote(data.results[0]);
-    });
-}*/
-
-
-
+export default Simpson;
